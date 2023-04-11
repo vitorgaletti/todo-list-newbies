dd3efee93cba803406282b248edd2039b6a7cd34
Vitor <vitor.fgaletti@gmail.com>
2023-04-11 15:27:50 -0300

    JS - Requisitos, funções e checkpoints pt.I

--- tasks.js
+++ tasks.js
@@ -16,3 +16,17 @@ let array = [1, 2, 3];
 const addTaskInput = document.getElementById("task_input");
 const addTaskButton = document.getElementsByTagName("button")[0];
 const taskList = document.getElementById("tasks_list");
+
+// TODO
+
+// add new task
+
+// complete task
+
+// incomplete task
+
+// delete task
+
+// sync HTML with taskData list
+
+// counter tasks
